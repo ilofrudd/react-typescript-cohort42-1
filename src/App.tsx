24694c59fda54549274ef3166ef6aebaac0ea0ac
ilofrudd <boyko5000@gmail.com>
2024-09-25 16:21:21 +0200

    HW06

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 // HOMEWORKS
 
 // LESSONS
-import Lesson_06 from "lessons/Lesson_06/Lesson_06";
+import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
 
 //CONSULTATIONS
 
 import "./App.css";
+import Homework_06 from "./homeworks/HomeWork_06/HomeWork_06";
 
 function App() {
   return (
     <div className="App">
       {/* LESSONS */}
-      <Lesson_06 />
+      {/* <Lesson_06 /> */}
+      {/* HomeWorks */}
+      <Homework_06 />
     </div>
   );
 }
---
+++ src/homeworks/HomeWork_06/HomeWork_06.tsx
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+interface Car {
+    brand: string;
+    price: number;
+    isDiesel: boolean;
+  }
+  
+  const cars: Car[] = [
+    { brand: "BMW", price: 20000, isDiesel: true },
+    { brand: "Mercedes", price: 22000, isDiesel: false },
+    { brand: "Porsche", price: 50000, isDiesel: true },
+    { brand: "Nissan", price: 25000, isDiesel: false },
+    { brand: "Audi", price: 50000, isDiesel: true },
+  ];
+  
+  const Homework_06: React.FC = () => {
+    return (
+      <div className="homework06-wrapper">
+        <h1 className="page-title">Homework 06</h1>
+        <div className="cards-container">
+          {cars.map((car, index) => (
+            <div className="card" key={index}>
+              <h2 className="card-title">{car.brand}</h2>
+              <p className="card-info">Price: €{car.price}</p>
+              <p className="card-info">
+                Fuel Type: {car.isDiesel ? "Diesel" : "Petrol"}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default Homework_06;
+
